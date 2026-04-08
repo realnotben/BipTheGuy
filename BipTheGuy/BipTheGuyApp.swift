d7b299eb2fd14e374f51148e3147890197b8ecb2
realnotben <benjaminfigueroa022@gmail.com>
2026-04-08 10:44:02 -0700

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by FIGUEROA, BENJAMIN on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
